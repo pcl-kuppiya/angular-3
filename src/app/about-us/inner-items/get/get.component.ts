a6ea7db76e6facc16d31fcaef646a77937101567
Gihanvs98 <darkcreationsl98@gmail.com>
2025-12-29 23:16:19 +0530

    feat:create 3 components

---
+++ src/app/about-us/inner-items/get/get.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get',
+  standalone: true,
+  imports: [],
+  templateUrl: './get.component.html',
+  styleUrl: './get.component.scss'
+})
+export class GetComponent {
+
+}
